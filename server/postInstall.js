0a7027091585ccb7c8cf855bd2478a98cb7fa997
Ctibor Laky <luckylooke@gmail.com>
2016-12-21 15:24:22 +0100

    postInstall enhancing jsdoc template, +webpack jsdoc tmp fix removed, not needed anymore

---
+++ server/postInstall.js
@@ -0,0 +1,35 @@
+var fs = require('fs'),
+
+    regexp = /<\?js if \(data\.defaultvalue\) \{\?>[^}]+<\?js } \?>/,
+    match,
+    newStr = '<?js if (data.customclass && data.customclass.length) {?> ' +
+                '<dt class="tag-default method-doc-label method-doc-details-label">Classes applied to &lt;HTML&gt;:</dt> ' +
+                '<dd class="tag-default"> ' +
+                    '<ul class="dummy"> ' +
+                        '<?js data.customclass.forEach(function(c) { ?> ' +
+                            '<li><?js= c.name ?> - <?js= c.description ?><?js if (c.demo) {?>(<a href="<?js= c.demo ?>">DEMO</a>)<?js } ?></li> ' +
+                        '<?js }); ?> ' +
+                    '</ul> ' +
+                '</dd> ' +
+            '<?js } ?>';
+
+fs.readFile(__dirname + '/../node_modules/ink-docstrap/template/tmpl/details.tmpl', 'utf-8', function(err, contents) {
+    if (err) console.error(err);
+
+    if(contents.match(/data\.customclass && data\.customclass\.length/))
+        return; // already fixed
+
+    match = contents.match(regexp);
+
+    if(!match)
+        return;
+
+    contents = contents.replace(match[0], match[0] + newStr);
+
+    fs.writeFile(__dirname + '/../node_modules/ink-docstrap/template/tmpl/details.tmpl', contents, function(err){
+        "use strict";
+        if(err) console.error(err);
+
+        console.log('file fix done: ', './node_modules/ink-docstrap/template/tmpl/details.tmpl');
+    });
+});
