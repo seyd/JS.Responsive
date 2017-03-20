2189ff6f566948de4ba5ffd45a718be7582124fe
Ctibor Laky <luckylooke@gmail.com>
2017-03-20 08:57:31 +0100

    more declarative defaultCfg

--- server/defaultCfg.js
+++ server/defaultCfg.js
@@ -1,1 +1,22 @@
-module.exports = parseInt('11000001110001', 2); // first '1' is protection  against loosing leading zeros, next '1's or '0's are flags
+var featureList = require(__dirname + '/featuresList.json');
+var defaultList = [
+
+	'breakpoints',
+	'detect-touch',
+	'document-state',
+	'focus-blur',
+	'touch-vs-mouse',
+
+];
+var cfg = '1'; // first '1' is protection  against loosing leading zeros, every next '1's or '0's are flags
+
+
+
+featureList.forEach( function ( feature ) {
+
+	cfg += defaultList.indexOf( feature.file ) > -1 ? 1 : 0;
+
+});
+
+
+module.exports = parseInt( cfg, 2 );
